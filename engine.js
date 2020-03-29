dd4fddfdc34a9bfdaf6f6b236d985194d2b7a39d
Adarsh <adroyalz@gmail.com>
2020-03-29 07:48:46 -0700

    Commented out engine stuff with errors, might need uncommenting

--- engine.js
+++ engine.js
@@ -13,36 +13,36 @@ const Engine = function(time_step, render, update) {
   this.render = render;
 
 
-  var
-    engine = module.exports = { games : {}, game_count:0 },
-    UUID        = require('node-uuid'),
-    verbose     = true;
+  // var
+  //   engine = module.exports = { games : {}, game_count:0 },
+  //   UUID        = require('node-uuid'),
+  //   verbose     = true;
 
-    //Since we are sharing code with the browser, we
-    //are going to include some values to handle that.
-    global.window = global.document = global;
+  //   //Since we are sharing code with the browser, we
+  //   //are going to include some values to handle that.
+  //   global.window = global.document = global;
 
-    //Import shared game library code.
-    require('./game.js');
+  //   //Import shared game library code.
+  //   require('./game.js');
 
-    //A simple wrapper for logging so we can toggle it,
-    //and augment it for clarity.
-    engine.log = function() {
-        if(verbose) console.log.apply(this,arguments);
-    };
+  //   //A simple wrapper for logging so we can toggle it,
+  //   //and augment it for clarity.
+  //   engine.log = function() {
+  //       if(verbose) console.log.apply(this,arguments);
+  //   };
 
-    engine.fake_latency = 0;
-    engine.local_time = 0;
-    engine._dt = new Date().getTime();
-    engine._dte = new Date().getTime();
-        //a local queue of messages we delay if faking latency
-    engine.messages = [];
+  //   engine.fake_latency = 0;
+  //   engine.local_time = 0;
+  //   engine._dt = new Date().getTime();
+  //   engine._dte = new Date().getTime();
+  //       //a local queue of messages we delay if faking latency
+  //   engine.messages = [];
 
-    setInterval(function(){
-        engine._dt = new Date().getTime() - engine._dte;
-        engine._dte = new Date().getTime();
-        engine.local_time += engine._dt/1000.0;
-    }, 4);
+  //   setInterval(function(){
+  //       engine._dt = new Date().getTime() - engine._dte;
+  //       engine._dte = new Date().getTime();
+  //       engine.local_time += engine._dt/1000.0;
+  //   }, 4);
 
 
 
